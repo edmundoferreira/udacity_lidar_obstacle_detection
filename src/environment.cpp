28b7ad33dfbaa90047eb4b130b0fa923e0feedf0
Edmundo Ferreira <edmundo.ferreira@esa.int>
2019-10-29 09:42:22 +0100

    First attempt to show point cloud segmentation
    - due to issues with PCL library in debian of double free or memory corruption had to compile it ubuntu

--- src/environment.cpp
+++ src/environment.cpp
@@ -49,10 +49,13 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     Lidar *lidarSen = new Lidar(cars, 0);
     pcl::PointCloud<pcl::PointXYZ>::Ptr pclData  = lidarSen->scan();
     // renderRays(viewer,lidarSen->position, pclData);
-    renderPointCloud(viewer, pclData, "point cloud", Color(1,1,1));
+    // renderPointCloud(viewer, pclData, "point cloud", Color(1,1,1));
 
     // TODO:: Create point processor
-
+    ProcessPointClouds<pcl::PointXYZ>* pointProcessor = new ProcessPointClouds<pcl::PointXYZ>();
+    std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor->SegmentPlane(pclData, 100, 0.2);
+    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1,0,0));
+    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0,1,0));
 }
 
 
