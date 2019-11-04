3f89465de0a9538c32728bcbef65670dd90bfa35
Edmundo Ferreira <edmundo.ferreira@esa.int>
2019-11-04 21:55:16 +0100

    Implements RANSAC for a plane
    - using 50 iteration and 0.2m for tolerance
    - used Eigen for point distances/difference and for vector cross product making it more readable

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -61,7 +61,7 @@ pcl::visualization::PCLVisualizer::Ptr initScene()
   	return viewer;
 }
 
-std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+std::unordered_set<int> RansacLine(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
 {
 	std::unordered_set<int> inliersResult;
 	std::unordered_set<int> inliersTemp;
@@ -120,6 +120,70 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 	return inliersResult;
 }
 
+std::unordered_set<int> RansacPlane(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+{
+	std::unordered_set<int> inliersResult;
+	std::unordered_set<int> inliersTemp;
+	srand(time(NULL));
+
+	Eigen::Vector3f p1;
+	Eigen::Vector3f p2;
+	Eigen::Vector3f p3;
+	// TODO: Fill in this function DONE
+
+	// For max iterations
+	for(auto iteration= 0; iteration<maxIterations; iteration++) {
+
+		// Randomly sample subset and fit line
+		auto index1 = rand() % cloud->points.size();
+		auto index2 = rand() % cloud->points.size();
+		auto index3 = rand() % cloud->points.size();
+		if (index1 != index2 && index1 != index3 && index2 != index3) //escape equal indexes ie single point line will fail
+		{
+			inliersTemp.clear();
+			inliersTemp.insert(index1);
+			inliersTemp.insert(index2);
+			inliersTemp.insert(index3);
+			p1 = cloud->points[index1].getVector3fMap();
+			p2 = cloud->points[index2].getVector3fMap();
+			p3 = cloud->points[index3].getVector3fMap();
+
+			auto v1 = p2 - p1;
+			auto v2 = p3 - p1;
+
+			auto vNormal= v1.cross(v2);
+
+			auto A = vNormal.x();
+			auto B = vNormal.y();
+			auto C = vNormal.z();
+			auto D = -(vNormal.x()*p1.x() + vNormal.y()*p1.y() + vNormal.z()*p1.z());
+
+			// Measure distance between every point and fitted line
+			for (auto index = 0; index < cloud->points.size(); index++) {
+				//escape p1 p2 that are inliers already
+				if(inliersTemp.count(index)>0)
+					continue;
+				// If distance is smaller than threshold count it as inlier
+				// d = |A*x+B*y+C*z+D|/sqrt(A^2+B^2+C^2).d=∣A∗x+B∗y+C∗z+D∣/sqrt(A^2 +B^2 +C^2)
+				auto d = std::abs(A*cloud->points[index].x + B*cloud->points[index].y + C*cloud->points[index].z + D) / std::sqrt(std::pow(A, 2) + std::pow(B, 2) + std::pow(C, 2));
+				if (d < distanceTol)
+				{
+					inliersTemp.insert(index);
+				}
+			}
+			// Return indicies of inliers from fitted line with most inliers
+			if(inliersTemp.size()>inliersResult.size())
+				inliersResult = inliersTemp;
+
+		}else{
+			iteration--;
+		}
+
+	}
+
+	return inliersResult;
+}
+
 int main ()
 {
 
@@ -127,11 +191,11 @@ int main ()
 	pcl::visualization::PCLVisualizer::Ptr viewer = initScene();
 
 	// Create data
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
 
 
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function DONE
-	std::unordered_set<int> inliers = Ransac(cloud, 50, 0.5);
+	std::unordered_set<int> inliers = RansacPlane(cloud, 50, 0.2);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
